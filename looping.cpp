6fd996d997c1a0d46f73e9dfd1ac2deca66eefd3
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-03-31 15:24:28 +0700

    tambah default pilihan tidak valid

--- looping.cpp
+++ looping.cpp
@@ -69,6 +69,8 @@ int main() {
             case 0:
                 cout << "Program selesai." << endl;
                 return 0;
+            default:
+                cout << "Pilihan tidak valid, silakan coba lagi." << endl;
         }
     }
     return 0;
