2d44186bfbb1d3984e18f795bb05b5e84a3a26d6
anisadzakiyyaha-glitch <anisadzakiyyah.a@gmail.com>
2026-03-31 14:49:00 +0700

    buat kerangka semua function

--- looping.cpp
+++ looping.cpp
@@ -7,3 +7,14 @@ using namespace std;
 // Variable global
 int angka;
 int pilihan;
+
+bool cekPrima() {}
+bool cekFibonacci() {}
+void inputAngka() {}
+void tampilHasilPrima() {}
+void tampilHasilFibonacci() {}
+void tampilMenu() {}
+
+int main() {
+    return 0;
+}
